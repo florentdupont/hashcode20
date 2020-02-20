7fa44cd42ad57643ea9e390f41cbb29e09868d18
Florent Dupont <florent.dupont@gmail.com>
2020-02-20 20:28:19 +0100

    a

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,18 +7,16 @@ fun main() {
     val resourcesPath = "/src/main/resources"
     val path = homePath + resourcesPath
 
-
    //  Process().run("$path/a_example.txt", "$path/a_example.out")
-    Process().run("$path/b_read_on.txt", "$path/b_read_on.out")
+//    Process().run("$path/b_read_on.txt", "$path/b_read_on.out")
 //    Process().run("$path/c_incunabula.txt", "$path/c_incunabula.out")
 //    Process().run("$path/d_tough_choices.txt", "$path/d_tough_choices.out")
 //    Process().run("$path/e_so_many_books.txt", "$path/e_so_many_books.out")
-//    Process().run("$path/f_libraries_of_the_world.txt", "$path/f_libraries_of_the_world.out")
+    Process().run("$path/f_libraries_of_the_world.txt", "$path/f_libraries_of_the_world.out")
 
 }
 
 class Process {
-
 
     val bookScores = hashMapOf<Int, Int>()
 
